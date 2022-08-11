cb8c8e0273ac1d8066274bd916a61f33d2c18aa9
zPierreB <pierre.briere1998@gmail.com>
2022-08-11 19:40:45 +0200

    working register + add db_Controller for a better organisation

--- back/index.js
+++ back/index.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const profils = require('./profil');
+const path = require('path');
 
 const connection = mysql.createConnection({
-    host    :'localhost',
-    user    :'root',
-    password:'',
-    database:'piguisfy'
+    host    :process.env.DB_HOST,
+    user    :process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_DATABASE
 });
 
 connection.connect();
@@ -18,6 +20,7 @@ const port = process.env.PORT || 8080;
 const app = express()
     .use(express.json())
     .use(express.urlencoded({ extended: false}))
+    .use(express.static(path.join(__dirname, 'static')))
     .use(cors())
     .use(bodyParser.json())
     .use(profils(connection));
--- back/profil.js
+++ back/profil.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 function createRouter(db) {
     //Mettre les routes ici
 
+    // add a new user into the DB
     router.post('/APIPiguisfy', (req, res, next) => {
-        console.log(req.body);
+        const salt = bcrypt.genSaltSync(10);
+        const pswd  =bcrypt.hashSync(req.body.password, salt);
         db.query(
             'INSERT INTO profil (email, username, password, country) VALUES (?,?,?,?)',
-            [req.body.email, req.body.username, req.body.password, req.body.country],
+            [req.body.email, req.body.username, pswd, req.body.country],
             (error) => {
                 if (error) {
                     console.log(error);
