e2c0b3f868183fc313528a4f6eec79e0bf79653f
zPierreB <pierre.briere1998@gmail.com>
2023-10-31 01:53:26 +0100

    add user check on back and front

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -8,32 +8,29 @@ export const login = async (req, res) => {
     const { email, password } = req.body
 
     if(email === "" || password === "") {
-        res.status(400).json({ message: 'Please provide an email and a password.' })
+        return res.status(400).json({ message: 'Please provide an email and a password.' })
     }
 
     await findOneByEmail(email)
     .then((user) => {
       if(user.length < 1) {
         res.status(401).json({ message: 'Provided informations are incorrect.' })
-        return
-      }
-      
-      if(!bcrypt.compareSync(password, user[0].password)) {
+        return    
+      } else if(!bcrypt.compareSync(password, user[0].password)) {
         res.status(401).json({ message: "Unable to authenticate the user" });
       } else {
-        const payload = { 'email': user[0].email }
+        const payload = { 'id': user[0].id, 'email': user[0].email }
         
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
           algorithm: "HS256",
           expiresIn: "6h",
         })
-        
         res.status(200).json({ authToken: authToken })
       }
     })
     .catch((error) => res.status(500).json({ message: `Internet Server Error: ${error}` }))
 }
-
+// TO DELETE BECAUSE USER IS CHECK IN MODEL
 export const verify = (req, res) => {
   res.status(200).json(req.payload)
 }
--- backend/middleware/isAuthenticate.js
+++ backend/middleware/isAuthenticate.js
@@ -2,24 +2,35 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { findOneByEmailAndId } from '../models/user.model.js'
+
 const isAuthenticated = (req, res, next) => {
 
   const token = getTokenFromHeaders(req);
   if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ message: 'Unauthorized token' });
   }
   
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.TOKEN_SECRET, async(err, decoded) => {
     if (err) {
-      return res.status(401).json({ message: 'Unauthorized' });
+      return res.status(401).json({ message: 'Unauthorized verify' });
     }
   
     req.payload = decoded;
+
+    const checkUser = await findOneByEmailAndId([req.payload.email, req.payload.id])
+    if(checkUser.length < 1) {
+      return res.status(401).json({ message: 'User uncheck' });
+    }
+
+    req.user = checkUser[0];
+    console.log('req.user: ', req.user)
     next();
   });
 };
 
 function getTokenFromHeaders(req) {
+  console.log(req.headers.authorization)
   if (
     req.headers.authorization &&
     req.headers.authorization.split(' ')[0] === 'Bearer'
@@ -31,4 +42,4 @@ function getTokenFromHeaders(req) {
   return null;
 }
 
-export default isAuthenticated;
+export default isAuthenticated; getTokenFromHeaders;
